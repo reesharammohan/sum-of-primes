885a87ee4dd707c98b4b75e11c9a1d85be97d620
reesharammohan <reeshamohan20@gmail.com>
2023-05-13 21:02:05 +1200

    Added unit test cases
    Added unit test cases to validate within and above range

---
+++ src/test/java/com/sample/prime/test/PrimeApplicationTests.java
@@ -0,0 +1,40 @@
+package com.sample.prime.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sample.prime.response.PrimeResponse;
+import com.sample.prime.service.PrimeService;
+
+@SpringBootTest
+public class PrimeApplicationTests {
+
+	@Autowired
+	PrimeService primeService;
+	
+	
+	@Test
+	public void testPrimeSum_ForLessThanTenMillion() {
+		
+		PrimeResponse expectedResponse = new PrimeResponse();
+		expectedResponse.setMessage("Sum Value calculated successfully!");
+		expectedResponse.setResult(3682913);
+		PrimeResponse actualResponse = primeService.getSumOfPrimes(1000);
+		assertEquals(expectedResponse.getResult(), actualResponse.getResult());
+		assertEquals(expectedResponse.getMessage(), actualResponse.getMessage());
+	}
+
+	@Test
+	public void testPrimeSum_ForMoreThanTenMillion() {
+		
+		PrimeResponse expectedResponse = new PrimeResponse();
+		expectedResponse.setMessage("Input value should be less than 10 million");
+		expectedResponse.setResult(0);
+		PrimeResponse actualResponse = primeService.getSumOfPrimes(10000000);
+		assertEquals(expectedResponse.getResult(), actualResponse.getResult());
+		assertEquals(expectedResponse.getMessage(), actualResponse.getMessage());
+	}
+}
